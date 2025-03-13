3a2ce7baa7a61683b2ff05050afac402c1629a4d
ClumsyPenguin <geerinckxseppe@gmail.com>
2025-03-13 22:22:21 +0100

    Added benchmark project

---
+++ Aspect Oriented Programming/Workshops/AOP/AOP.Benchmark/Program.cs
@@ -0,0 +1,100 @@
+﻿using AOP.Interception.WeatherForecast.Config;
+using AOP.Interception.WeatherForecast.Services;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using BenchmarkDotNet.Analysers;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ZiggyCreatures.Caching.Fusion;
+
+namespace AOP.Benchmark;
+
+public class Config : ManualConfig
+{
+    public Config()
+    {
+        AddLogger(ConsoleLogger.Default);
+        AddColumnProvider(DefaultColumnProviders.Instance);
+
+        AddExporter(HtmlExporter.Default);
+        AddExporter(CsvExporter.Default);
+        AddExporter(RPlotExporter.Default);
+
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddAnalyser(EnvironmentAnalyser.Default);
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<WeatherForecastBenchmark>(new Config());
+    }
+}
+
+[SimpleJob(invocationCount: 1)]
+public class WeatherForecastBenchmark
+{
+    private IWeatherForecastService _interceptedService = null!;
+    private IWeatherForecastService _plainService = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var interceptedApp = CreateWebApp(interceptorsEnabled: true);
+        _interceptedService = interceptedApp.Services.GetRequiredService<IWeatherForecastService>();
+            
+        var plainApp = CreateWebApp(interceptorsEnabled: false);
+        _plainService = plainApp.Services.GetRequiredService<IWeatherForecastService>();
+    }
+
+    [Benchmark]
+    public async Task InterceptedCall()
+    {
+        for (var i = 0; i < 1000; i++)
+        {
+            await _interceptedService.GetWeatherForecast();
+        }
+    }
+
+    [Benchmark]
+    
+    public async Task PlainCall()
+    {
+        for (var i = 0; i < 1000; i++)
+        {
+            await _plainService.GetWeatherForecast();
+        }
+    }
+
+    private static WebApplication CreateWebApp(bool interceptorsEnabled)
+    {
+        var builder = WebApplication.CreateBuilder();
+            
+        builder.Services.AddMemoryCache();
+        builder.Services.AddFusionCache().WithRegisteredMemoryCache();
+        
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+        builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+        {
+            if (interceptorsEnabled) 
+                GeneralDiConfigLoader.LoadWithInterceptors(containerBuilder);
+            else 
+                GeneralDiConfigLoader.LoadWithoutInterceptors(containerBuilder);
+        });
+
+        var app = builder.Build();
+
+        return app;
+    }
+}
--- Aspect Oriented Programming/Workshops/AOP/AOP.Interception/WeatherForecast/Config/GeneralDiConfigLoader.cs
+++ Aspect Oriented Programming/Workshops/AOP/AOP.Interception/WeatherForecast/Config/GeneralDiConfigLoader.cs
@@ -1,3 +1,5 @@
+using AOP.Interception.WeatherForecast.Repositories;
+using AOP.Interception.WeatherForecast.Services;
 using Autofac;
 
 namespace AOP.Interception.WeatherForecast.Config;
@@ -5,6 +7,17 @@ namespace AOP.Interception.WeatherForecast.Config;
 public static class GeneralDiConfigLoader
 {
     public static void Load(ContainerBuilder builder)
+    {
+        builder.RegisterModule<DiConfig>();
+    }
+
+    public static void LoadWithoutInterceptors(ContainerBuilder builder)
+    {
+        builder.RegisterType<WeatherForecastService>().As<IWeatherForecastService>();
+        builder.RegisterType<WeatherForecastRepository>().As<IWeatherForecastRepository>();
+    }
+
+    public static void LoadWithInterceptors(ContainerBuilder builder)
     {
         builder.RegisterModule<DiConfig>();
     }
